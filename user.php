382a9ab54fe498d0b4ea1f2ad0bc6a5d0f507f07
miya-algoma <miya@algomau.ca>
2025-05-11 18:13:05 +0000

    Add userExists() and create_user()
    Added userExists() to check for existing usernames and create_user() to insert new users into the database with placeholder password (hashing will be added later).

--- user.php
+++ user.php
@@ -11,5 +11,17 @@ class User {
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function userExists($username) {
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM users WHERE username = ?");
+        $statement->execute([$username]);
+        return $statement->rowCount() > 0;
+    }
+
+    public function create_user($username, $password) {
+        $db = db_connect();
+        $statement = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        return $statement->execute([$username, $password]); // hashed password will come in next commit
+    }
 }
 ?>
