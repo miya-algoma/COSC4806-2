c49a643bc7f64dca02b2d5bdfb78ec2c230e1dde
miya-algoma <miya@algomau.ca>
2025-05-17 13:57:18 +0000

    Added Course description in database and config

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 // Mark Iya 219600000
+//COSC4806 Assignment 2
 define('DB_HOST', '7rduj.h.filess.io');
 define('DB_USER', 'COSC4806_steeplabor');
 define('DB_PASS', '97c9057c43a24d5c2939a8f0e12c3d84a763ca73');
--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php
 // Mark Iya 219600000
+//COSC4806 Assignment 2
 require_once('config.php');
 
 function db_connect() {
